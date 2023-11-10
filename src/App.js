067c336203fc9762e84a07c6ac74533f385ad4c1
Georgui716371 <azelps071del@gmail.com>
2023-11-10 10:13:04 -0500

    Refactored code to improve performance and readability.

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React from "react";
+import styled from "styled-components";
+import Table  from "./Table";
+import makeData from "./makeData";
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+
+      input {
+        font-size: 1rem;
+        padding: 0;
+        margin: 0;
+        border: 0;
+      }
+    }
+  }
+
+  .pagination {
+    padding: 0.5rem;
+  }
+`;
+
+
+
+function App() {
+  const [data, setData] = React.useState(() => makeData(20));
+  const [originalData] = React.useState(data);
+  const [skipPageReset, setSkipPageReset] = React.useState(false);
+
+  const updateMyData = (rowIndex, columnId, value) => {
+    setSkipPageReset(true);
+    setData((old) =>
+      old.map((row, index) => {
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value
+          };
+        }
+        return row;
+      })
+    );
+  };
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        columns: [
+          {
+            Header: "First Name",
+            accessor: "firstName"
+          },
+          {
+            Header: "Last Name",
+            accessor: "lastName"
+          }
+        ]
+      },
+      {
+        Header: "Info",
+        columns: [
+          {
+            Header: "Age",
+            accessor: "age"
+          },
+          {
+            Header: "Visits",
+            accessor: "visits"
+          },
+          {
+            Header: "Status",
+            accessor: "status"
+          },
+          {
+            Header: "Profile Progress",
+            accessor: "progress",
+            Cell: (cellProps) => {
+              let valor = cellProps.value;
+              let row = cellProps.row.index;
+              let column = cellProps.column.id;
+              const [value, setValue] = React.useState(valor);
+
+              const onChange = (e) => {
+                setValue(e.target.value);
+              };
+
+              const onBlur = () => {
+                updateMyData(row, column, value);
+              };
+
+              React.useEffect(() => {
+                setValue(valor);
+              }, [valor]);
+              return (
+                <input value={value} onChange={onChange} onBlur={onBlur} />
+              );
+            }
+          }
+        ]
+      }
+    ],
+    []
+  );
+
+  React.useEffect(() => {
+    setSkipPageReset(false);
+  }, [data]);
+
+  const resetData = () => setData(originalData);
+
+  return (
+    <Styles>
+      <button onClick={resetData}>Reset Data</button>
+      <Table
+        columns={columns}
+        data={data}
+        updateMyData={updateMyData}
+        skipPageReset={skipPageReset}
+      />
+    </Styles>
+  );
+}
+
+export default App;
